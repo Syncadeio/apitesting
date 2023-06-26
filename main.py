baf612beb20b84aa3ae286de44eed6234a714360
adelgadorios <delgadoale00@protonmail.com>
2023-06-26 13:48:56 -0400

    flask

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import g4f
 from PyPDF2 import PdfReader
 import tiktoken 
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
 
 def pdf_to_text(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
@@ -16,8 +19,8 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
-pdf_path = 'syllabus.pdf'
-syllabus_text = pdf_to_text(pdf_path)
+
+
 
 
 token_count = num_tokens_from_string(syllabus_text, "cl100k_base")
@@ -25,13 +28,20 @@ print(f"Number of tokens in syllabus text: {token_count}")
 if token_count > 7900:
     print("token count exceeded")
     exit()
-prompt = f"Given the following college syllabus, extract the relevant dates and events:{syllabus_text} \n Format your response in ics format. "
 
-# streamed completion
-response = g4f.ChatCompletion.create(model='gpt-4', messages=[
+
+@app.route('/api', methods=['POST'])
+def my_api():
+    pdf_path = request.files['file']
+    pdf_path = request.get_json()  # get data sent to the API
+    #pdf_path = 'syllabus.pdf'
+    syllabus_text = pdf_to_text(pdf_path)
+    prompt = f"Given the following college syllabus, extract the relevant dates and events:{syllabus_text} \n Format your response in ics format. "
+    # streamed completion
+    response = g4f.ChatCompletion.create(model='gpt-4', messages=[
                                      {"role": "user", "content": prompt}], stream=False)
-infile = open("response.ics", 'w')
-print(response)
-response = '\n'.join(response.split('\n')[1:]).strip()
-infile.write(response)
-infile.close()
+    response = response
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)  # run the Flask app
