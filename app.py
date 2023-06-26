819242209700e62ea073db090f6c222b53ff9c94
adelgadorios <delgadoale00@protonmail.com>
2023-06-26 14:52:01 -0400

    update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import tiktoken
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-
 def pdf_to_text(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PdfReader(pdf_file)
@@ -19,28 +18,23 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
-
-
-
-
-token_count = num_tokens_from_string(syllabus_text, "cl100k_base")
-print(f"Number of tokens in syllabus text: {token_count}")
-if token_count > 7900:
-    print("token count exceeded")
-    exit()
-
-
 @app.route('/api', methods=['POST'])
 def my_api():
-    pdf_path = request.files['file']
-    pdf_path = request.get_json()  # get data sent to the API
-    #pdf_path = 'syllabus.pdf'
-    syllabus_text = pdf_to_text(pdf_path)
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+
+    syllabus_text = pdf_to_text(file)
+    token_count = num_tokens_from_string(syllabus_text, "cl100k_base")
+    print(f"Number of tokens in syllabus text: {token_count}")
+    if token_count > 7900:
+        return jsonify({'error': 'Token count exceeded'}), 400
+
     prompt = f"Given the following college syllabus, extract the relevant dates and events:{syllabus_text} \n Format your response in ics format. "
-    # streamed completion
-    response = g4f.ChatCompletion.create(model='gpt-4', messages=[
-                                     {"role": "user", "content": prompt}], stream=False)
-    response = response
+    response = g4f.ChatCompletion.create(model='gpt-4', messages=[{"role": "user", "content": prompt}], stream=False)
     return jsonify(response), 200
 
 if __name__ == '__main__':
